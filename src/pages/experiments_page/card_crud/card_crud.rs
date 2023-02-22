945fcf118b0304fbd387b041f1388713bc7da246
Daehwan Lee <redface0512@gmail.com>
2023-02-21 23:48:10 -0800

    moved button to form children

--- src/pages/experiments_page/card_crud/card_crud.rs
+++ src/pages/experiments_page/card_crud/card_crud.rs
@@ -20,6 +20,8 @@ pub struct CardFormEntity {
 pub struct CardFormProps {
     pub onchange: Callback<CardFormEntity>,
     pub default_fields: Option<CardFormEntity>,
+    #[prop_or_default]
+    pub children: Children,
 }
 
 #[function_component]
@@ -51,12 +53,19 @@ pub fn CardForm(_props: &CardFormProps) -> Html {
         })
     };
 
-    let card_form_data = CardFormEntity { title: (*title_state).clone(), content: (*content_state).clone() };
-    _props.onchange.emit(card_form_data.clone());
+    let title_state_cloned = title_state.clone();
+    let content_state_cloned = content_state.clone();
+    let onchange_cloned = _props.onchange.clone();
+    use_effect_with_deps(move |_| {
+        let card_form_data = CardFormEntity { title: (*title_state_cloned).clone(), content: (*content_state_cloned).clone() };
+        onchange_cloned.emit(card_form_data);
+    }, [title_state.clone(), content_state.clone()]);
+
     html! {
         <form>
-            <label>{"title: "}<input ref={input_node_ref} onchange={onchange_input} value={card_form_data.title.clone()} /></label>
-            <label>{"content: "}<textarea ref={textarea_node_ref}  onchange={onchange_textarea} value={card_form_data.content.clone()} /></label>
+            <label>{"title: "}<input ref={input_node_ref} onchange={onchange_input} value={(*title_state).clone()} /></label>
+            <label>{"content: "}<textarea ref={textarea_node_ref}  onchange={onchange_textarea} value={(*content_state).clone()} /></label>
+            {_props.children.clone()}
         </form>
     }
 }
@@ -68,14 +77,36 @@ pub fn CardCrud() -> Html {
         title: String::from(""),
         content: String::from(""),
     });
-    let _card_form_data_cloned = card_form_data.clone();
 
+    let card_form_data_cloned = card_form_data.clone();
     let onchange = use_callback(move |form_data, _| {
         card_form_data.set(form_data);
-    }, _card_form_data_cloned.clone());
+    }, ());
+
+    let cards_state: UseStateHandle<Vec<CardEntity>> = use_state(|| vec![
+        CardEntity { title: String::from("title1"), content: String::from("content1"), date: Utc::now() },
+        CardEntity { title: String::from("title2"), content: String::from("content2"), date: Utc::now() },
+    ]);
+
+    let cards_state_cloned = cards_state.clone();
+    let onclick = {
+        Callback::from(move |_| {
+            let _card_form_data_cloned = card_form_data_cloned.clone();
+            let _cards_state_cloned = cards_state_cloned.clone();
+            let mut cards_cloned: Vec<_> = _cards_state_cloned.to_vec();
+            let _title = (*_card_form_data_cloned).title.clone();
+            let _content = (*_card_form_data_cloned).content.clone();
+            let new_card = CardEntity { title: _title, content: _content, date: Utc::now() };
+            cards_cloned.push(new_card);
+            _cards_state_cloned.set(cards_cloned);
+        })
+    };
+
     let display_card_form = if *show_new_card == true {
         html! {
-            <CardForm {onchange} />
+            <CardForm {onchange}>
+                <button {onclick} type="button">{"Add"}</button>
+            </CardForm>
         }
     } else {
         html! {
@@ -83,28 +114,10 @@ pub fn CardCrud() -> Html {
         }
     };
 
-    let cards_state: UseStateHandle<Vec<CardEntity>> = use_state(|| vec![
-        CardEntity { title: String::from("title1"), content: String::from("content1"), date: Utc::now() },
-        CardEntity { title: String::from("title2"), content: String::from("content2"), date: Utc::now() },
-    ]);
-
-    let onclick = {
-        let cards_state_cloned = cards_state.clone();
-        Callback::from(move |_| {
-            let mut cards_cloned: Vec<_> = cards_state_cloned.to_vec();
-            let _title = (*_card_form_data_cloned).title.clone();
-            let _content = (*_card_form_data_cloned).content.clone();
-            let new_card = CardEntity { title: _title, content: _content, date: Utc::now() };
-            cards_cloned.push(new_card);
-            cards_state_cloned.set(cards_cloned);
-        })
-    };
-
     let edit_mode_idx: UseStateHandle<Option<i32>> = use_state(|| None);
     let card_comps = (*cards_state).iter().enumerate().map(|(idx, props)| {
         let edit_mode_idx_cloned = edit_mode_idx.clone();
         let onclick = Callback::from(move |_: _| {
-            info!("card clicked: {:?}", idx);
             edit_mode_idx_cloned.set(Some(idx.try_into().unwrap()));
         });
         let onchange = Callback::from(|_: _| {});
@@ -145,13 +158,11 @@ pub fn CardCrud() -> Html {
             </li>
         }
     }).collect::<Html>();
-    info!("cards {:?}", card_comps);
 
     html! {
         <div>
             {display_card_form}
             <div>
-                <button {onclick}>{"Add"}</button>
                 <ol>
                     {card_comps}
                 </ol>
